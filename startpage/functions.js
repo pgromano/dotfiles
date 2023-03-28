c8e04ae4d4392f879b0dcdf5770141ad84c8ebad
Pablo Romano <pgromano@users.noreply.github.com>
2023-03-28 15:19:25 -0500

    fixed bug in mod

--- startpage/functions.js
+++ startpage/functions.js
@@ -4,21 +4,21 @@ function clock() {
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
-    var ampm = hours > 12 ? 'PM' : 'AM'
+    var ampm = hours >= 12 ? 'PM' : 'AM'
 
-    hours = ('0' + hours % 12).slice(-2);
+    // modulo for hour 12 needs to be shifted to 12 
+    // otherwise, the results should be as is
+    hours = hours % 12 === 0 ? 12 : hours % 12
+
+    // cast the timestamps to zero-padded strings
+    hours = ('0' + hours).slice(-2);
     minutes = ('0' + minutes).slice(-2);
     seconds = ('0' + seconds).slice(-2);
 
+    // pretty printing is the thing to do!
     let timestr = hours + ":" + minutes + " " + ampm;
     document.getElementById("clock").innerHTML = timestr
     setTimeout(clock, 500);
-}
-
-function runSearch() {
-    let query = document.getElementById('searchBar').innerText
-    console.log(query)
-    window.location.href = "https://www.google.com/search/?q=" + query
 }
 
 clock();
